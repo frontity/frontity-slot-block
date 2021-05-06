9c455f90a291b5b1925d83a2c95afe2f3d6fa8e9
Luis Herranz <luisherranz@gmail.com>
2021-05-06 16:21:11 +0200

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dd53f16ed985a295d271fda9320aecc2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '697da6890380d528a866e57f9e70f71c');
