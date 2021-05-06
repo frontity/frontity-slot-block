b63f34ba3c90b3377101671f187559f46a0017ce
Luis Herranz <luisherranz@gmail.com>
2021-05-06 16:13:24 +0200

    Remove front css and add icon

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
-/**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
- */
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from "@wordpress/blocks";
+import { next } from "@wordpress/icons";
+import edit from "./edit";
+import save from "./save";
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './style.scss';
-
-/**
- * Internal dependencies
- */
-import Edit from './edit';
-import save from './save';
-
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
- */
-registerBlockType( 'frontity/frontity-slot', {
-	/**
-	 * @see ./edit.js
-	 */
-	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
+registerBlockType("frontity/frontity-slot", {
+	icon: next,
+	edit,
 	save,
-} );
+});
